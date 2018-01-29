9ded082144005f63ad8df1b2ac051e94980dcfdf
Alex <asavchenkov1@gmail.com>
2018-01-28 17:16:00 -0800

    fixed code for calc divergence. tests choose strat
    Basically, if we choose and/or based off of popcounts, what is the most
    we can expect things to diverge?
    
    also the old code was wrong. Just alternating results in divergence to
    one extreme, not to alternating extremes.

--- calc_divergence.py
+++ calc_divergence.py
@@ -1,8 +1,12 @@
 
 cur = 0.5
-
 for i in range(100):
-    cur = 1-(cur*cur)
-    print(i,cur)
+    if(cur>0.5):
+        operation = 'and'
+        cur = cur*cur
+    else:
+        operation = 'or '
+        cur = 1-( (1-cur) * (1-cur))
+    print(i,operation, cur)
 
 
